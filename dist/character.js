ac8808627175906b721ecc3eea54b272f2f60fa1
Tarta35 <caballeroluciano352@gmail.com>
2025-06-12 12:43:24 -0300

    pusheo por si acaso, ajustes de api

---
+++ dist/character.js
@@ -0,0 +1,12 @@
+export class Character {
+    constructor(name, characterClass, level, hp, mana, attack, items) {
+        this.name = name;
+        this.characterClass = characterClass;
+        this.level = level;
+        this.hp = hp;
+        this.mana = mana;
+        this.attack = attack;
+        this.items = items;
+    }
+}
+//# sourceMappingURL=character.js.map
